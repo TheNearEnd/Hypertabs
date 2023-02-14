9b8476a2fa952b3eaf8e5c1667a2b874403d73c2
urasdbwd <102772348+urasdbwd@users.noreply.github.com>
2023-02-13 18:55:06 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
-
-// The following message MAY NOT be removed
-console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
-
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server;
@@ -45,4 +41,4 @@ server.on('listening', () => {
 	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
-server.listen({ port: (process.env.PORT || 8080) })
+server.listen({ port: (process.env.PORT || 80) })
